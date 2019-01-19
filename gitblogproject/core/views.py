af4ee1069ad10ba9bc6a7e848a0eccb7f072d9fa
crowley <CrowleyDzn@hotmail.com>
2019-01-19 07:26:40 -0200

    First commit

--- gitblogproject/core/views.py
+++ gitblogproject/core/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from core.models import Post
+from django.core.paginator import Paginator
 
 def home(request):
 	return render(request, 'core/index.html')
 
 def lista(request):
-	lista = Post.objects.all().order_by('-id')
-	return render(request,'core/lista.html',{'lista_post': lista})
+	#lista = Post.objects.all().order_by('-id')
+	lista = Post.objects.all()
+	paginator = Paginator(lista, 3)
+	page = request.GET.get('page')
+	posts = paginator.get_page(page)
+	return render(request,'core/lista.html',{'posts': posts,'lista_post': posts})
+	#return render(request,'core/lista.html',{'lista_post': posts})
